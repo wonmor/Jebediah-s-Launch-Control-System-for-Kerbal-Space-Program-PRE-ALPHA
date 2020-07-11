e7ebbb7a84c6a035e03fae2c2fd83f82dc9a678c
John Seong <wonmor@gmail.com>
2020-07-11 22:22:53 +0900

    Alpha Build 1.1

--- houston.ino
+++ houston.ino
@@ -1,6 +1,6 @@
 /*
    Jebediah's Launch Control System for Kerbal Space Program
-   Alpha Build 1.0
+   Alpha Build 1.1
    An Open-Source Project by John Seong
 */
 
@@ -21,7 +21,7 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {12, 11, 10, 9};
 byte colPins[COLS] = {8, 7, 6};
 
-Keypad keypad=Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
@@ -33,21 +33,26 @@ void lcdShow(const char *line0, const char *line1) {
   lcd.print(line1);
 }
 
+void lcdShowResult(float result) {
+  lcd.print(result);
+}
+
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
   lcdShow("Jeb's Launch", "Control System");
   delay(3000);
-  lcdShow("Alpha Build 1.0", " STUDIO HORIZON");
+  lcdShow("Alpha Build 1.1", " STUDIO HORIZON");
   delay(3000);
   lcdShow("Welcome, Kerman", "Press any key...");
   while (!keypad.getKey()) ;  // wait for key press
 }
 
 void loop() {
-  static enum {START, MENU, SET_COUNT, COUNT_DOWN, TWR} state = START;
+  static enum {START, MENU, SET_COUNT, COUNT_DOWN, SET_THRUST, SET_WEIGHT, TWR_COMPUTE} state = START;
   static uint32_t last_second;  // millis() value on last full second
-  static int count;             // countdown value
+  static int count, thrust, weight;             // countdown, thrust, weight value
+  static float ratio;
 
   char key = keypad.getKey();
 
@@ -55,15 +60,16 @@ void loop() {
     case START:  // transitional state
       lcdShow("1. Countdown", "2. TWR");
       state = MENU;
-      /* fallthrough */
+    /* fallthrough */
     case MENU:
       if (key == '1') {  // Countdown
-        lcdShow("Enter time", "seconds: ");
+        lcdShow("Countdown Timer", "Seconds: ");
         count = 0;
         state = SET_COUNT;
       } else if (key == '2') {  // TWR
-        lcdShow("Thrust:", "# to continue...");
-        state = TWR;
+        lcdShow("TWR Calculator", "Thrust: ");
+        thrust = 0, weight = 0, ratio = 0;
+        state = SET_THRUST;
       }
       break;
     case SET_COUNT:
@@ -91,14 +97,32 @@ void loop() {
           break;
         }
         lcd.setCursor(1, 1);
-        lcd.print(count<10 ? "  " : count<100 ? " " : "");  // pad
+        lcd.print(count < 10 ? "  " : count < 100 ? " " : ""); // pad
         lcd.print(count);
       } else if (key == '*') {
         state = START;
       }
       break;
-    case TWR:
-      if (key == '*' || key == '#') {
+    case SET_THRUST:
+      if (key >= '0' && key <= '9' && thrust <= 9999) {
+        lcd.print(key);
+        thrust = 10 * thrust + (key - '0');
+      } else if (key == '#') {
+        lcdShow("TWR Calculator", "Weight: ");
+        state = SET_WEIGHT;
+      } else if (key == '*') {
+        state = START;
+      }
+      break;
+    case SET_WEIGHT:
+      if (key >= '0' && key <= '9' && weight <= 9999) {
+        lcd.print(key);
+        weight = 10 * weight + (key - '0');
+      } else if (key == '#') {
+        lcdShow("Thrust-to-weight", "ratio: ");
+        ratio = thrust / weight;
+        lcdShowResult(ratio);
+      } else if (key == '*') {
         state = START;
       }
       break;
